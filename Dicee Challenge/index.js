45453ec40ac3b52adf2c3d4dc311d89e1e551434
Trevor Douglas <tdoug19@gmail.com>
2021-02-22 14:51:20 -0600

    Dicee game done.

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,25 +1,31 @@
-var randomNumber = Math.floor(Math.random() * 5);
-randomNumber++;
-console.log(randomNumber);
-
-var image1 = document.querySelector("img.img1");
-console.log(image1);
-//document.querySelector(".img1").setAttribute('src','images/dice2.png')
-
+var randomNumber1 = (Math.floor(Math.random() * 5)+1);
+var randomNumber2 = (Math.floor(Math.random() * 5)+1);
 
 var diceImage;
 
-diceImage = randomDiceImage(randomNumber);
+diceImage = randomDiceImage(randomNumber1);
 console.log(diceImage);
 
 document.querySelector("img.img1").setAttribute('src', diceImage);
-
-randomNumber = (Math.floor(Math.random() * 5) +1);
-diceImage = randomDiceImage(randomNumber);
+diceImage = randomDiceImage(randomNumber2);
 console.log(diceImage);
-
 document.querySelector("img.img2").setAttribute('src', diceImage);
 
+if(randomNumber1 > randomNumber2)
+{
+  //Player 1 wins
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randomNumber1 == randomNumber2)
+{
+  //tie
+  document.querySelector("h1").innerHTML = "It's a TIE";
+}
+else
+{
+  //Player 2 wins.
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
 
 function randomDiceImage(imageNum)
 {
