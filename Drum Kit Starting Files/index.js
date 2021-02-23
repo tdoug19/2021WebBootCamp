d26a394363fe0ea93feee6cc794ff143756b0a2c
Trevor Douglas <tdoug19@gmail.com>
2021-02-23 12:19:58 -0600

    finished sounds

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,9 +3,7 @@ var audio = new Audio("sounds/tom-1.mp3");
 for (var i = 0; i < document.querySelectorAll(".drum").length; ++i)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-      console.log(this.innerHTML);
 
-      
       switch(this.innerHTML){
         case "w":
           new Audio("sounds/tom-1.mp3").play();
@@ -22,6 +20,17 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; ++i)
           new Audio("sounds/tom-4.mp3").play();
         break;
 
+        case "j":
+          new Audio("sounds/snare.mp3").play();
+        break;
+
+        case "k":
+          new Audio("sounds/kick-bass.mp3").play();
+        break;
+
+        case "l":
+          new Audio("sounds/crash.mp3").play();
+        break;
         default:
 
 
