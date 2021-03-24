4e3fef0981ad446d6785bf11c0fb93ef2349fef8
Trevor Douglas <tdoug19@gmail.com>
2021-03-24 09:51:18 -0600

    challenge 6

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -23,6 +23,19 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/about", (req, res) => {
+
+  res.render('about',{
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", (req, res) => {
+
+  res.render('contact',{
+    contactContent: contactContent
+  });
+});
 
 
 
